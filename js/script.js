0259d07e362cb3fffa437dbc8c1215dbffa8aa10
Igor <example@gmail.com>
2020-10-02 22:22:18 +0300

    added axios

--- js/script.js
+++ js/script.js
@@ -128,12 +128,6 @@ showTabContent();
             //modal.classList.toggle('show');
             document.body.style.overflow='';
         }  
-
-    
-            
-    
-    
-
     // закрываем страницу по нажатию на любую область 
     // страницы за пределами modal__dialog
     modal.addEventListener('click',(e)=>{
@@ -198,30 +192,22 @@ class MenuCard{
        this.parent.append(element);
     }
 }
-new MenuCard(
-"img/tabs/vegy.jpg","vegy",
-"Меню 'Фитнес'",
-'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.'+
-' Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-7,'.menu .container'
-).render();
-new MenuCard(
-"img/tabs/elite.jpg","elite",
-'Меню “Премиум”',
-'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение'+
-' блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-12,'.menu .container','menu__item','big'
-).render();
-new MenuCard(
-"img/tabs/post.jpg","post",
-'Меню "Постное"',
-'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного'+
-' происхождения, молоко из миндаля, овса, кокоса или гречки,'+
-' правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-8,'.menu .container','menu__item'
-).render();
+const getResource = async (url)=>{
+    const res = await fetch(url);
+    if(!res.ok){
+       throw new Error(`Could not fetch${url}, status: ${res.status}`);
+    }
 
-// Отправляем данные на сервер
+    return await res.json();
+};
+// Добавляем работу с библиотекой axios
+axios.get('http://localhost:3000/menu')
+.then(data=>{
+    data.data.forEach(({img,altimg,title,descr,price})=>{
+        new MenuCard(img,altimg,title,descr,price, '.menu .container').render();
+    });
+});
+
 const forms=document.querySelectorAll('form');
 const message={
     loading:'img/form/spinner.svg',
@@ -230,11 +216,20 @@ const message={
 
 };
 forms.forEach(item =>{
-    postData(item);
+    bindPostData(item);
 });
-      function postData(form){
-        // У каждой кнопки есть свойство submit
-        // В первую очередь убераем стандартное поведение браузера
+const postData = async (url,data)=>{
+    const res = await fetch(url,{
+        method:'POST',
+        headers:{
+         'Content-type':'application/json'
+        },
+        body:data
+    });
+    return await res.json();
+};
+
+      function bindPostData(form){
         form.addEventListener('submit',(e)=>{
           e.preventDefault();
           const statusMessage=document.createElement('img');
@@ -248,18 +243,9 @@ forms.forEach(item =>{
 
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
-            fetch('server.php',{
-                method:'POST',
-                headers:{
-                    'Content-type':'application/json'
-                    },
-                body:JSON.stringify(object)
-                })
-                .then(data=>data.text())
+            const json=JSON.stringify(Object.fromEntries(formData.entries())); 
+
+                postData('http://localhost:3000/requests',json)
                 .then(data=>{
                     console.log(data);
                     showThanksModal(message.success);  
@@ -298,9 +284,9 @@ forms.forEach(item =>{
               closeModal();
           },4000);
       }
-    fetch('http://localhost:3000/menu')
-    .then(data=>data.json())
-    .then(res=>console.log(res));
+    // fetch('http://localhost:3000/menu')
+    // .then(data=>data.json())
+    // .then(res=>console.log(res));
 });
 
 
